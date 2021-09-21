4d20cb542949678f8ddbc161438b3c3f0014009d
Jay Koontz <jkoontz2010@gmail.com>
2021-09-21 14:32:02 -0500

    form filler!

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,23 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+function fillForm(fakeData) {
+  const formElements = document.querySelectorAll('form')[1].elements;
+  const names = ['firstname', 'lastname', 'reg_email__', 'reg_passwd__'];
+
+  names.forEach((name) => {
+    document.querySelectorAll('form')[1].elements[name].value = fakeData[name];
+  });
+}
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  switch (message.type) {
+    case 'fillform':
+      console.log('filling!');
+      fillForm(message.fakeData);
+      alert('done');
+
+      break;
+  }
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -2,23 +2,49 @@ import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import { useEffect, useState } from 'react';
+
+const fakeData1 = {
+  firstname: 'matt',
+  lastname: 'k',
+  reg_email__: 'asf@gmail.com',
+  reg_passwd__: 'asdfasdf',
+  sex: 'true'
+};
 
 const Popup = () => {
+  const [fakeData, setFakeData] = useState()
+
+  useEffect(() => {
+    fetch(
+      `https://localhost:3000/fakedata`,
+      {
+        method: "GET",
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        setFakeData(response)
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  function fillForm() {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, {type:"fillform", fakeData}, function(response){
+        console.log("fin")
+    });
+  });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        {fakeData ? (
+        <button onClick={fillForm}>Fill me</button>
+        ) : (
+          <div>loading</div>
+        )
+        }
       </header>
     </div>
   );
